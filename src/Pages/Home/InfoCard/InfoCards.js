4dc1a30c60202e36984028e2057c34fb7e4a439e
Sunan13208 <sunanrabbi1918@gmail.com>
2024-09-11 11:48:56 +0600

    last

--- src/Pages/Home/InfoCard/InfoCards.js
+++ src/Pages/Home/InfoCard/InfoCards.js
@@ -5,33 +5,33 @@ import marker from '../../../assets/icons/marker.svg'
 import InfoCard from './InfoCard';
 
 const InfoCards = () => {
-    const datas=[
+    const datas = [
         {
-            id:1,
-            title:"Opening Hours",
-            descri:"Open 9.00AM to 5.00PM everyday",
-            img:clock,
-            bgClass:"bg-gradient-to-r from-primary to-secondary"
+            id: 1,
+            title: "Opening Hours",
+            descri: "Open 9.00AM to 5.00PM everyday",
+            img: clock,
+            bgClass: "bg-gradient-to-r from-primary to-secondary"
         },
         {
-            id:2,
-            title:"Visit Our Location",
-            descri:"Octroi mor,Kazla,Rajshahi",
-            img:marker,
-            bgClass:"bg-accent"
+            id: 2,
+            title: "Visit Our Location",
+            descri: "191/A, Monnafer Mor, Rajshahi",
+            img: marker,
+            bgClass: "bg-accent"
         },
         {
-            id:3,
-            title:"Contact us now",
-            descri:"+8801875899096",
-            img:phone,
-            bgClass:"bg-gradient-to-r from-primary to-secondary"
+            id: 3,
+            title: "Contact us now",
+            descri: "+8801533692833",
+            img: phone,
+            bgClass: "bg-gradient-to-r from-primary to-secondary"
         },
     ]
     return (
         <div className='grid mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {
-                datas.map(data=><InfoCard key={data.id} data={data}></InfoCard>)
+                datas.map(data => <InfoCard key={data.id} data={data}></InfoCard>)
             }
         </div>
     );
--- src/Router/Router/Router.js
+++ src/Router/Router/Router.js
@@ -14,28 +14,33 @@ import SignUp from "../../Pages/SignUp/SignUp";
 import AdminRouter from "../AdminRouter/AdminRouter";
 import PrivateRouter from "../PrivateRouter/PrivateRouter";
 
-const router =createBrowserRouter([
-    {path:'/',element:<Main></Main>,
-    errorElement:<DisPlayError></DisPlayError>,
-    children:[
-        {path:'/',element:<Home></Home>},
-        {path:'/login',element:<Login></Login>},
-        {path:'/signup',element:<SignUp></SignUp>},
-        {path:'/appointment',element:<PrivateRouter><Appointment></Appointment></PrivateRouter>},
-    ]},
-    {path:'/dashboard',element:<PrivateRouter><DashBoardLayout></DashBoardLayout></PrivateRouter>,
-    errorElement:<DisPlayError></DisPlayError>
-    ,children:[
-        {path:'/dashboard',element:<Dashboard></Dashboard>},
-        {path:'/dashboard/users',element:<AdminRouter><AllUser></AllUser></AdminRouter>},
-        {path:'/dashboard/doctors',element:<AdminRouter><AllDoctor></AllDoctor></AdminRouter>},
-        {path:'/dashboard/mangedoctors',element:<AdminRouter><ManageDoctor></ManageDoctor></AdminRouter>},
-        {path:'/dashboard/payment/:id',
-        element:<AdminRouter><Payment></Payment></AdminRouter>,
-        loader:({params})=>fetch(`https://doctor-portal-server-lyart.vercel.app/payment/${params.id}`)
-    }
+const router = createBrowserRouter([
+    {
+        path: '/', element: <Main></Main>,
+        errorElement: <DisPlayError></DisPlayError>,
+        children: [
+            { path: '/', element: <Home></Home> },
+            { path: '/login', element: <Login></Login> },
+            { path: '/signup', element: <SignUp></SignUp> },
+            { path: '/appointment', element: <PrivateRouter><Appointment></Appointment></PrivateRouter> },
+        ]
+    },
+    {
+        path: '/dashboard', element: <PrivateRouter><DashBoardLayout></DashBoardLayout></PrivateRouter>,
+        errorElement: <DisPlayError></DisPlayError>
+        , children: [
+            { path: '/dashboard', element: <Dashboard></Dashboard> },
+            { path: '/dashboard/users', element: <AdminRouter><AllUser></AllUser></AdminRouter> },
+            { path: '/dashboard/doctors', element: <AdminRouter><AllDoctor></AllDoctor></AdminRouter> },
+            { path: '/dashboard/mangedoctors', element: <AdminRouter><ManageDoctor></ManageDoctor></AdminRouter> },
+            {
+                path: '/dashboard/payment/:id',
+                element: <PrivateRouter><Payment></Payment></PrivateRouter>,
+                loader: ({ params }) => fetch(`https://doctor-portal-server-lyart.vercel.app/payment/${params.id}`)
+            }
 
-    ]}
+        ]
+    }
 ])
 
 export default router
